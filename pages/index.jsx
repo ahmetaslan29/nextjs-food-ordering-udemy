4bdeb78435ddb1302397d4b5f5f861151d0875fc
Ahmet ASLAN <54741924+ahmetaslan29@users.noreply.github.com>
2023-06-12 11:35:29 +0300

    Github example

--- pages/index.jsx
+++ pages/index.jsx
@@ -10,7 +10,7 @@ export default function Index() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <h1 className='text-center h-screen bg-primary'>Food Ordering</h1>
+      <h1 className='text-center h-screen bg-primary'>Food Ordering example</h1>
       <Home />
     </div>
   )
