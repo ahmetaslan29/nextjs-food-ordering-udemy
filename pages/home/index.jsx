d4db77857ff20664e7b4e1cc1b1d3a6bdb6e43f5
Ahmet ASLAN <ahmetaslan2997@gmail.com>
2024-08-04 23:32:40 +0300

    master push some changes

--- pages/home/index.jsx
+++ pages/home/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const Index = () => {
-  return  <div className='text-red-600 font-dancing text-center text-3xl'>Feane Home</div>
+  return  <div className='text-red-600 font-dancing text-center text-3xl'>Feane Home Example</div>
 }
 
 export default Index
